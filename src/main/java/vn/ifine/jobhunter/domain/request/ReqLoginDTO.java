1d683ef027082c18d03de41b776d0a7cc3dc3d04
ifine2002 <boyyhp1@gmail.com>
2024-12-21 15:18:10 +0700

    Add Dependency OAuth 2

---
+++ src/main/java/vn/ifine/jobhunter/domain/request/ReqLoginDTO.java
@@ -0,0 +1,15 @@
+package vn.ifine.jobhunter.domain.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ReqLoginDTO {
+    @NotBlank(message = "username không được để trống")
+    private String username;
+
+    @NotBlank(message = "password không được để trống")
+    private String password;
+}
